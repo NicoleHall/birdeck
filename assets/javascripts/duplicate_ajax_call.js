b91d1c7c9a7308198fc48df8b4a38eb069cc3792
Nicole Hall <nicolealexandrahall@gmail.com>
2016-01-11 10:20:24 -0700

    did a duplicate ajax get call as practice

---
+++ assets/javascripts/duplicate_ajax_call.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+function renderPost(post){
+  $('#latest-posts').append(
+    "<div class='post'><h6>Posted at: " +
+     post.created_at +
+     "</h6><p>"+ post.description +
+     "</p></div>"
+    )
+}
+
+ $.ajax({type: 'GET',
+        url: 'https://turing-birdie.herokuapp.com/api/v1/posts.json',
+        success: function(posts){
+        $.each(posts, function(index, post){
+          renderPost(post)
+        })
+       }
+      })
+
+
+})
